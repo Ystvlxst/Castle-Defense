21b6548bf867eef2520fcc3cd8b42af34707bea0
n1f1 <84967079+n1f1@users.noreply.github.com>
2022-09-09 18:59:14 +0300

    Laser fix

--- Assets/Scripts/Weapon/BallisticWeapon.cs
+++ Assets/Scripts/Weapon/BallisticWeapon.cs
@@ -1,64 +1,39 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using DG.Tweening;
+using BabyStack.Model;
 using UnityEngine;
 
-public class WeaponWithoutBallistics : Weapon
+public class BallisticWeapon : Weapon, IModificationListener<float>
 {
     [SerializeField] private Transform _spawn;
     [SerializeField] private Transform _weaponTransform;
-    [SerializeField] private float _speed;
+    [SerializeField] private float _angle;
     [SerializeField] private Bullet _template;
     [SerializeField] private TargetSelector _targetSelector;
     [SerializeField] private StackPresenter _stackPresenter;
     [SerializeField] private float _cooldown;
     [SerializeField] private int _shotsPerAmmo;
     [SerializeField] private ParticleSystem _shotEffect;
-    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _torqueForce;
 
+    private readonly float _g = Physics.gravity.y;
     private int _ammo;
-    private float _timePerAmmo = 5;
     private float _cooldownFactor = 1;
-    private Enemy _target;
 
-    private void Start() =>
+    private void Start() => 
         StartCoroutine(Shoot());
-
-    private void Update()
-    {
-        Rotate();
-    }
 
     private IEnumerator Shoot()
     {
-        float time = 0;
-
         while (true)
         {
-            yield return null;
-            
-            if (CanRefillAmmo())
+            if(CanRefillAmmo())
                 RefillAmmo();
-
-            if(CanShoot() && (_target == null || _target.IsDying))
-                _target = _targetSelector.SelectEnemyTarget();
-
-            while (CanShoot() && _target != null)
+            
+            while (CanShoot())
             {
-                if(_target.IsDying)
-                    break;
+                Shot();
                 
-                Shot(_target.transform.position);
-                time += Time.deltaTime;
-
-                if (time > _timePerAmmo)
-                {
-                    _ammo--;
-                    time = 0;
-                }
-
                 yield return new WaitForSeconds(_cooldown / _cooldownFactor);
             }
 
@@ -66,20 +41,10 @@ public class WeaponWithoutBallistics : Weapon
         }
     }
 
-    private void Rotate()
-    {
-        if(_target == null)
-            return;
-
-        Vector3 targetDirection = _target.transform.position - Vector3.up * 0.5f - _weaponTransform.position;
-        
-        _spawn.Rotate(Vector3.Cross(targetDirection, _spawn.forward), _rotationSpeed * Time.deltaTime);
-    }
-
     private bool CanShoot() => 
         _ammo > 0 && _targetSelector.HasTarget;
 
-    private bool CanRefillAmmo() =>
+    private bool CanRefillAmmo() => 
         _stackPresenter.Empty == false && _ammo == 0;
 
     private void RefillAmmo()
@@ -90,12 +55,29 @@ public class WeaponWithoutBallistics : Weapon
         _ammo = _shotsPerAmmo;
     }
 
-    private void Shot(Vector3 targetPosition)
+    private void Shot()
     {
+        _ammo--;
         _shotEffect.Play();
+        
+        _spawn.localEulerAngles = new Vector3(-_angle, 0f, 0f);
+
+        Vector3 fromTo = _targetSelector.SelectTarget() - _weaponTransform.position;
+        Vector3 fromToXZ = new Vector3(fromTo.x, 0f, fromTo.z);
+
+        _weaponTransform.rotation = Quaternion.LookRotation(fromToXZ, Vector3.up);
+
+        float x = fromToXZ.magnitude;
+        float y = fromTo.y;
+
+        float angleInRadians = _angle * Mathf.PI / 180;
+
+        float v2 = (_g * x * x) / (2 * (y - Mathf.Tan(angleInRadians) * x) * Mathf.Pow(Mathf.Cos(angleInRadians), 2));
+        float v = Mathf.Sqrt(Mathf.Abs(v2));
 
         Bullet bullet = Instantiate(_template, _spawn.position, Quaternion.identity);
-        bullet.Rigidbody.velocity = _spawn.forward * _speed;
+        bullet.Rigidbody.velocity = _spawn.forward * v;
+        bullet.Rigidbody.AddTorque(_spawn.forward * _torqueForce);
     }
 
     public void OnModificationUpdate(float value)
--- Assets/Scripts/Weapon/TargetSelectors/LaserTargetSelector.cs
+++ Assets/Scripts/Weapon/TargetSelectors/LaserTargetSelector.cs
@@ -5,23 +5,9 @@ using UnityEngine;
 
 public class LaserTargetSelector : TargetSelector
 {
-    private Enemy _lastSelected;
-
-    public override Vector3 SelectTarget()
-    {
-        Enemy first = EnemyContainer.Enemies.First();
-
-        if (first == _lastSelected && EnemyContainer.Enemies.Count() > 1)
-            first = EnemyContainer.Enemies.ElementAt(Random.Range(0, EnemyContainer.Enemies.Count()));
-
-        _lastSelected = first;
-        Vector3 target = first.transform.position;
-
-        if (!first.IsDying)
-            target += first.transform.forward;
-
-        target.z = Mathf.Clamp(target.z, first.Target.transform.position.z, target.z);
-
-        return target;
-    }
+    public override Vector3 SelectTarget() => 
+        EnemyContainer.Enemies.ElementAt(Random.Range(0, EnemyContainer.Enemies.Count())).transform.position;
+    
+    public override Enemy SelectEnemyTarget() => 
+        EnemyContainer.Enemies.ElementAt(Random.Range(0, EnemyContainer.Enemies.Count()));
 }
