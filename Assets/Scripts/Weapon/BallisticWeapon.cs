edda0631a9a5add1700c636ed59c926d8d433994
n1f1 <84967079+n1f1@users.noreply.github.com>
2022-09-15 14:11:27 +0300

    Weapon rotation

--- Assets/Scripts/Weapon/BallisticWeapon.cs
+++ Assets/Scripts/Weapon/BallisticWeapon.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using BabyStack.Model;
+using BehaviorDesigner.Runtime.Tasks.Unity.UnityVector3;
 using UnityEngine;
 using DG.Tweening;
 
@@ -15,24 +16,27 @@ public class BallisticWeapon : Weapon
     private readonly float _g = Physics.gravity.y;
     private Vector3 _selectTarget;
     private Vector3 _targetDirection;
+    private float _maxDegreesDelta = 20f;
+    private float _maxMagnitudeDelta = 20f;
 
-    private void Start() => 
+    private void Start() =>
         StartCoroutine(Shoot());
 
     private void Update() =>
-            Rotate();
+        Rotate();
 
     private IEnumerator Shoot()
     {
         while (true)
         {
-            if(CanRefillAmmo())
-                RefillAmmo((int)(ShotsPerAmmo * UpgradeFactor));
+            if (CanRefillAmmo())
+                RefillAmmo((int) (ShotsPerAmmo * UpgradeFactor));
 
             while (CanShoot())
             {
                 _selectTarget = TargetSelector.SelectTarget();
 
+                yield return new WaitUntil(() => _targetDirection.normalized == Spawn.forward);
                 Shot();
 
                 yield return new WaitForSeconds(_cooldown / UpgradeFactor);
@@ -44,25 +48,22 @@ public class BallisticWeapon : Weapon
 
     private void Shot()
     {
-        if (_targetDirection == _selectTarget)
-        {
-            Vector3 fromTo = _selectTarget - WeaponTransform.position;
-            Vector3 fromToXZ = new Vector3(fromTo.x, fromTo.y * 0.5f, fromTo.z);
+        Vector3 fromTo = _selectTarget - WeaponTransform.position;
+        Vector3 fromToXZ = new Vector3(fromTo.x, fromTo.y * 0.5f, fromTo.z);
 
-            float x = fromToXZ.magnitude;
-            float y = fromTo.y;
+        float x = fromToXZ.magnitude;
+        float y = fromTo.y;
 
-            float angleInRadians = _angle * Mathf.PI / 180;
+        float angleInRadians = _angle * Mathf.PI / 180;
 
-            float v2 = (_g * x * x) / (2 * (y - Mathf.Tan(angleInRadians) * x) * Mathf.Pow(Mathf.Cos(angleInRadians), 2));
-            float v = Mathf.Sqrt(Mathf.Abs(v2));
+        float v2 = (_g * x * x) / (2 * (y - Mathf.Tan(angleInRadians) * x) * Mathf.Pow(Mathf.Cos(angleInRadians), 2));
+        float v = Mathf.Sqrt(Mathf.Abs(v2));
 
-            Bullet bullet = Instantiate(_bulletTemplate, Spawn.position, Quaternion.identity);
-            bullet.Rigidbody.velocity = Spawn.forward * v;
-            bullet.Rigidbody.AddTorque(Spawn.forward * _torqueForce);
-            MinusAmmo();
-            ShotEffect.Play();
-        }
+        Bullet bullet = Instantiate(_bulletTemplate, Spawn.position, Quaternion.identity);
+        bullet.Rigidbody.velocity = Spawn.forward * v;
+        bullet.Rigidbody.AddTorque(Spawn.forward * _torqueForce);
+        MinusAmmo();
+        ShotEffect.Play();
     }
 
     private void Rotate()
@@ -73,7 +74,9 @@ public class BallisticWeapon : Weapon
         _targetDirection = _selectTarget - Vector3.up * 0.5f - WeaponTransform.position;
 
         float rotationSpeed = _rotationSpeed * UpgradeFactor * UpgradeFactor * Time.deltaTime;
-        Spawn.Rotate(Vector3.Cross(Spawn.forward, _targetDirection), rotationSpeed);
+        var rotation = Vector3.RotateTowards(Spawn.forward, _targetDirection, _maxDegreesDelta * Mathf.Deg2Rad * Time.deltaTime, _maxMagnitudeDelta);
+        
+        Spawn.rotation = Quaternion.LookRotation(rotation);
     }
 
     private void OnDrawGizmos()
