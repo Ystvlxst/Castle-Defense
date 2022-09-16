3f8833023f2e23f3a7d92e1ca5c58e8deb69a188
n1f1 <84967079+n1f1@users.noreply.github.com>
2022-09-16 14:40:18 +0300

    Big weapon

--- Assets/Scripts/Weapon/Gunpoint.cs
+++ Assets/Scripts/Weapon/Gunpoint.cs
@@ -7,7 +7,7 @@ public class Gunpoint : MonoBehaviour
     
     public void Shoot(Bullet bulletTemplate, float force, float torqueForce)
     {
-        Bullet bullet = Instantiate(bulletTemplate, transform.position, Quaternion.identity);
+        Bullet bullet = Instantiate(bulletTemplate, transform.position, transform.rotation);
         bullet.Rigidbody.velocity = transform.forward * force;
         bullet.Rigidbody.AddTorque(transform.forward * torqueForce);
         Shot?.Invoke();
